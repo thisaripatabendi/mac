4d5512a89a1ca77bae35e55de74e84fc335842a3
thisaripatabendi <thisaripatabendi@gmail.com>
2017-04-20 15:15:17 +0530

    Initial commit

---
+++ feature/feature/src/main/resources/dbscripts/mssql.sql
@@ -0,0 +1,9 @@
+
+-- -----------------------------------------------------
+-- Table `mac_DEVICE`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS mac_DEVICE (
+  mac_DEVICE_ID VARCHAR(45) NOT NULL ,
+  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (mac_DEVICE_ID) );
+
